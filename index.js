052c6987809bdfc8527138efbd4ced9e0c9d3ce8
objectundefined <gabriel.lipson@gmail.com>
2013-07-23 21:44:02 -0700

    different child proc setup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var fs = require('fs');
 var _ = require('underscore');
 var fork = require('child_process').fork;
 var format = require('util').format;
@@ -11,21 +12,66 @@ env['PORT'] = 8080 ;
 
 if ( nDeploy && _.isArray(nDeploy.processes) ) {
 	
-	nDeploy.processes.forEach(function(file){
+	nDeploy.processes.forEach(function(proc){
 		
-		var proc = path.join(sourcePath,file);
+		var file , keepAlive ;
 		
-		var child = fork( proc  ,[], {
+		if ( _.isString( proc ) ) {
+			
+			file = path.join( sourcePath , proc ) ;
+			
+		} else if ( _.isObject( proc ) && proc.file ) {
+			
+			file = path.join( sourcePath , proc.file ) ;
+			
+			keepAlive = proc.keepAlive || false ;
+			
+		} else {
+			
+			console.error("Process %s not valid." , JSON.stringify(proc) );
+			return;
+		}
 		
-			env : _.extend({},process.env,env) ,
-			cwd : sourcePath
-		
-		});
-		
-		child.on('exit',function(){
-			console.error('%s exited. Restarting services.',file);
-		})
+		if ( fs.existsSync( file ) ) {
+			
+			var child = new Child( file , keepAlive );
+			
+		} else {
+			
+			console.error("File %s does not exist.",file);
+			
+		}
 		
 	});
 	
 }
+
+function Child(proc,keepAlive){
+	
+	this.createChild(proc,keepAlive);
+	
+	return this ;
+	
+};
+
+Child.prototype.createChild = function(proc,keepAlive){
+	
+	var _this = this ;
+	_this.process = fork( proc  ,[], {
+		
+		env : _.extend({},process.env,env) ,
+		cwd : sourcePath
+		
+	});
+
+	if ( keepAlive ) {
+		
+		_this.process.on('exit',function(){
+			console.log( "Child %s exited. Restarting." , proc )
+			_this.process = _this.createChild( proc , keepAlive );
+			
+		})
+		
+	}
+	
+};
